6a9b8944bc70ec9cef89518aa7c54b6604aa6413
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-06-02 21:23:35 -0300

    feat: initial commit

---
+++ src/main/kotlin/com/pedros/passinv2/Passinv2Application.kt
@@ -0,0 +1,11 @@
+package com.pedros.passinv2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Passinv2Application
+
+fun main(args: Array<String>) {
+    runApplication<Passinv2Application>(*args)
+}
---
+++ src/test/kotlin/com/pedros/passinv2/Passinv2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pedros.passinv2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Passinv2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
